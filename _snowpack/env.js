e8dc4ef2b5cb8e666cc43192747c297bfb0e15aa
johnb8005 <johnb8005@users.noreply.github.com>
2021-08-11 23:32:04 +0000

    deploy: d631d22ba0ff89f25e604af55a1a07d9f9b1cb21

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "efefd37a7acd3e9721338018aa300381e1e3f7e8";
-export const SNOWPACK_PUBLIC_URL = "/johnb8005/realestate";
+export const SNOWPACK_PUBLIC_GIT_SHA = "d631d22ba0ff89f25e604af55a1a07d9f9b1cb21";
+export const SNOWPACK_PUBLIC_URL = "/realestate";
 export const MODE = "development";
 export const NODE_ENV = "development";
 export const SSR = false;
